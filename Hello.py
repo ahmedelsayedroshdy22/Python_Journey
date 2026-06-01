c56e83b67664abfe71a68148d934c4242d2914c3
ahmedelsayedroshdy22 <ahmed.zayed@orange.com>
2026-06-01 11:20:48 +0300

    Added a new api Call to our portal

--- Hello.py
+++ Hello.py
@@ -1,9 +1,29 @@
 
-Name = "968-maria, (D@t@ Engineer ) ;; 27y  "
+=import requests
 
-New= Name.strip().replace("968-", "").replace("@", "a").replace(";;", "").replace("(", "").replace(")", "").split(",")
-Age = New[1].strip().split("  ")
+url = "https://api.voxbone.com/v1/inventory/did"
+
+headers = {
+    "apikey": "Axxxx",
+    "Accept": "application/json"
+}
+
+params = {
+    "pageNumber": 0,
+    "pageSize": 1,
+    # optional filters - uncomment what you need
+     "countryCodeA3": "GBR",
+    # "serviceType": "VOXDID",  # or "VOX800" for toll-free
+    # "e164Pattern": "%25123",  # numbers containing 123
+    # "needAddressLink": False,
+    # "smsOutbound": False,
+    # "webRtcEnabled": False,
+}
+
+response = requests.get(url, headers=headers, params=params)
+
+print(f"Status: {response.status_code}")
 
 
-
-print(f"Age: {Age}")
+Data = response.json()
+print(Data['dids'][0]['otherOptions'])
